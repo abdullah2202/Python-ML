f8f64fda99980bb28b1250d7ea95907b43cfcf7f
Mohammad Abdullah via Desktop <abdullah_22@hotmail.com>
2022-07-12 13:55:58 +0100

    Added Multi Algorithms

--- training_set.py
+++ training_set.py
@@ -2,14 +2,21 @@
 
 from random import randint
 from re import X
-from sklearn.linear_model import LinearRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import StratifiedKFold, cross_val_score, train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 train_set_limit = 1000
-train_set_count = 100
+train_set_count = 150
 
 train_input = list()
 train_output = list()
 
+# Create Training Set
 for i in range(train_set_count):
     a = randint(0, train_set_limit)
     b = randint(0, train_set_limit)
@@ -17,6 +24,30 @@ for i in range(train_set_count):
     op = a + (4*b) + (3*c)
     train_input.append([a,b,c])
     train_output.append(op)
+
+x_train, x_validation, y_train, y_validation = train_test_split(train_input, train_output, test_size=0.20, random_state=1)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear',multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+# Evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=1, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+
+
 
 predictor = LinearRegression(n_jobs=-1)
 predictor.fit(X=train_input, y=train_output)
